dbddba0f4116796e985644989b64231ac4dd970c
sallyman128 <salmaan.arif.ali@gmail.com>
2023-03-17 19:38:59 -0400

    first commit

---
+++ Chess/Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Salmaan Ali on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
